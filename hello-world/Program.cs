69ce4a306a46b3bf469553006429aa8710818d1c
George Afrifa <sarpongk247@gmail.com>
2025-03-22 12:05:03 +0000

    comments, variables and constants done

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -6,7 +6,28 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            //Console.WriteLine("This is a writeline method");
+            //Console.WriteLine("This is another writeline Method");
+            //Console.Write("This is a write method");
+            //Console.Write("This is another write method");
+
+            // COMMENTS
+            // Use '//' for single line comments
+            // Use '/* */' for multi line comments
+
+            // VARIABLES - they can be changed, they can be declared without a value
+            string role;
+            int age = 30;
+            bool isActive = true;
+            double accountBalance = 125770.59;
+
+            // CONSTANT VARIABLES - they cannot be changed
+            const string NAME = "Kwadwo Sarpong";
+            const char SEX = 'M';
+            const bool IS_DEVELOPER = true;
+            const string DESIGNATION = "Chief Technical Office";
+
+            Console.WriteLine(NAME + " is a " + SEX + " working as the " + DESIGNATION + " of Hubtel Inc. He is " + age + " years of age and has a bank account balance of " + accountBalance + ".");
         }
     }
 }
